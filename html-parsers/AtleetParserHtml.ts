efd576d1d05d08282b3291188ff03540d98c8719
Niels Dequeker <niels.dequeker@fastmail.com>
2018-09-01 21:54:48 +0200

    Add source URL

--- html-parsers/AtleetParserHtml.ts
+++ html-parsers/AtleetParserHtml.ts
@@ -1,3 +1,5 @@
+// URL https://www.atletiek.nu/atleet/main/504453/
+
 export default `
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
