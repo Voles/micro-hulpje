fa301bbf596e0af8ed9421155b9378e45ff2d4ae
Niels Dequeker <niels.dequeker@fastmail.com>
2018-09-02 14:38:14 +0200

    Add URL validation

--- content-loaders/UrlContentLoader.ts
+++ content-loaders/UrlContentLoader.ts
@@ -4,6 +4,10 @@ import * as request from 'request';
 class UrlContentLoaderAdapter implements IContentLoader {
     load(url: string): Promise<string> {
         return new Promise<string>((resolve, reject) => {
+            if (!this.isUrlValid(url)) {
+                return reject(`De link '${url}' is ongeldig.`)
+            }
+
             request(url, function (error, response, html) {
                 if (error || response.statusCode !== 200) {
                     reject(error);
@@ -12,6 +16,15 @@ class UrlContentLoaderAdapter implements IContentLoader {
                 resolve(html);
             });
         });
+    }
+
+    private isUrlValid(url: string): boolean {
+        try {
+            new URL(url);
+            return true
+        } catch (error) {
+            return false
+        }
     }
 }
 
