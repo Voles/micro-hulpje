a9a17070dcc4938f44e0125e695ca4a51deaf01d
Niels Dequeker <niels.dequeker@fastmail.com>
2018-09-02 14:38:40 +0200

    Clean URLs + catch errors

--- index.ts
+++ index.ts
@@ -2,15 +2,7 @@ import MicroHulpje from "./domain/MicroHulpje"
 import FileWriter from "./writers/FileWriter";
 
 const STARTLIJSTEN = [
-    'https://www.atletiek.nu/wedstrijd/startlijst/208295/13/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/208299/1/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/208296/163/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/208298/7/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/208297/16/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/211784/13/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/211788/1/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/211789/7/',
-    'https://www.atletiek.nu/wedstrijd/startlijst/211790/16/'
+    ''
 ];
 
 const fileWriter = new FileWriter()
@@ -21,4 +13,7 @@ STARTLIJSTEN
         microHulpje
             .deelnemersOverviewVoorStartlijst(url)
             .then(overview => microHulpje.writeAsCsv(overview, `./output/${overview.titel}.csv`))
+            .catch(error => {
+                console.log(error);
+            })
     )
