a2e7ad16cb2dc72bb2127e9b7a6dae5feee35a5c
Niels Dequeker <niels.dequeker@fastmail.com>
2018-09-12 16:48:03 +0200

    Add caching to WedstrijdTijdsschemasService

--- app/services/WedstrijdTijdsschemasService.ts
+++ app/services/WedstrijdTijdsschemasService.ts
@@ -1,15 +1,17 @@
 import UrlContentLoaderAdapter from "../content-loaders/UrlContentLoader";
 import WedstrijdTijdsschemaModel from "../models/WedstrijdTijdsschemaModel";
 import WedstrijdTijdsschemaParser from "../html-parsers/WedstrijdTijdsschemaParser";
+import CacheService from "./CacheService";
 
 class WedstrijdTijdsschemasService {
     private urlContentLoader: UrlContentLoaderAdapter = new UrlContentLoaderAdapter()
     private wedstrijdTijdsschemasParser: WedstrijdTijdsschemaParser = new WedstrijdTijdsschemaParser()
+    private cache: CacheService = CacheService.getInstance()
 
     fromUrl(url: string): Promise<WedstrijdTijdsschemaModel> {
         return this
-            .urlContentLoader
-            .load(url)
+            .cache
+            .getOrSet(url, () => this.urlContentLoader.load(url))
             .then(html => this.wedstrijdTijdsschemasParser.parse(html))
     }
 }
