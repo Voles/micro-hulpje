0b423731d655af4d0ec3bb5f881c24a379737a22
Niels Dequeker <niels.dequeker@fastmail.com>
2018-09-12 15:06:50 +0200

    Rework tests for strings

--- app/utils/strings.test.ts
+++ app/utils/strings.test.ts
@@ -1,21 +1,23 @@
 import { obpRawToSortable, parseAfstandRawToNumber, parseTijdRawToNumber, removeDoubleSpaces } from "./strings";
 
-describe('converting raw OBP to sortable', () => {
-    it('should convert a distance', () => {
+describe('converting raw OBP to sortable number', () => {
+    it('should detect a formatted distance', () => {
         expect(obpRawToSortable('8,20')).toEqual(8.2)
     })
 
-    it('should convert a duration', () => {
+    it('should detect a formatted duration', () => {
         expect(obpRawToSortable('01:10,15')).toEqual(70.15)
     })
 })
 
-it('should convert an afstand', () => {
-    expect(parseAfstandRawToNumber('8,20')).toEqual(8.2)
-})
+describe('converting formatted results', () => {
+    it('should convert an afstand to a number', () => {
+        expect(parseAfstandRawToNumber('8,20')).toEqual(8.2)
+    })
 
-it('should convert a duration', () => {
-    expect(parseTijdRawToNumber('01:10,15')).toEqual(70.15)
+    it('should convert a duration to a number', () => {
+        expect(parseTijdRawToNumber('01:10,15')).toEqual(70.15)
+    })
 })
 
 it('should remove double spaces', () => {
